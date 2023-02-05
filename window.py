3d32d52ac9a9e7fbe922a1059a91366cedf029ba
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2023-02-06 02:15:11 +0800

    Implement New Elements in Window class
    - Reduce label spacing from 30 >> 10
    - remove location parameter from the CalendarButton in create_date_input method [line 39]
    - reformat code
    - align confirm, cancel button to the right
    - add create_date_input
    - add create_combobox
    - add create_time_selector

--- window.py
+++ window.py
@@ -1,15 +1,37 @@
 import PySimpleGUI as sg
 
+
 class Window(sg.Window):
-    LABEL_SPACING = 30
+    LABEL_SPACING = 10
     TEXT_SIZE = 50
 
     def __init__(self, title="Scheduler"):
         super().__init__(title)
-        layout = [[Window.create_label_input("Title")],
-                  [Window.create_label_input("Description", multiline=True)],
-                  [Window.create_date_input("Date")],
-                  [sg.Button("Confirm", key="-CONFIRM-"), sg.Button("Cancel", key="-CANCEL-")]]
+        _days = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ]
+        layout = [
+            [Window.create_label_input("Title")],
+            [Window.create_label_input("Description", multiline=True)],
+            [Window.create_combobox("Day", items=_days, default_item=_days[0])],
+            [Window.create_time_selector("Start Time")],
+            [
+                Window.create_time_selector(
+                    "Duration", active_selection=[True, True, False]
+                )
+            ],
+            [
+                sg.Stretch(),
+                sg.Button("Confirm", key="-CONFIRM-"),
+                sg.Button("Cancel", key="-CANCEL-"),
+            ],
+        ]
         self.layout(layout)
 
     @staticmethod
@@ -17,13 +39,18 @@ class Window(sg.Window):
         if placeholder is None:
             placeholder = ""
         if key is None:
-            key = f"-{text.upper()}-"
+            key = f"-{text.strip().replace(' ', '_').upper()}-"
 
         label = sg.Text(text, size=(Window.LABEL_SPACING, 1))
         if multiline:
-            entry = sg.Multiline(placeholder, autoscroll=True, size=(Window.TEXT_SIZE, 3), key=key)
+            entry = sg.Multiline(
+                placeholder,
+                autoscroll=True,
+                size=(Window.TEXT_SIZE, 3),
+                key=key,
+            )
         else:
-            entry = sg.Input(placeholder, size=(Window.TEXT_SIZE,1), key=key)
+            entry = sg.Input(placeholder, size=(Window.TEXT_SIZE, 1), key=key)
 
         return label, entry
 
@@ -32,13 +59,60 @@ class Window(sg.Window):
         if placeholder is None:
             placeholder = ""
         if key is None:
-            key = f"-{text.upper()}-"
+            key = f"-{text.strip().replace(' ', '_').upper()}-"
 
         label = sg.Text(text, size=(Window.LABEL_SPACING, 1))
         entry = sg.Input(placeholder, key=key)
-        cal_entry = sg.CalendarButton(text, close_when_date_chosen=True, target=key, location=(0, 0), no_titlebar=False)
+        cal_entry = sg.CalendarButton(
+            text,
+            close_when_date_chosen=True,
+            target=key,
+            no_titlebar=False,
+            format="%d-%m-%Y",
+        )
         return label, entry, cal_entry
-        
+
+    @staticmethod
+    def create_combobox(text, key=None, items=None, default_item=None):
+        if key is None:
+            key = f"-{text.strip().replace(' ', '_').upper()}-"
+        if items is None:
+            items = []
+
+        label = sg.Text(text, size=(Window.LABEL_SPACING, 1))
+        entry = sg.Combo(values=items, default_value=default_item, readonly=True)
+        return label, entry
+
+    @staticmethod
+    def create_time_selector(text, keys=None, active_selection=None):
+        if keys is None:
+            keys = [
+                f"-{text.strip().replace(' ', '_').upper()}_H-",
+                f"-{text.strip().replace(' ', '_').upper()}_M-",
+                f"-{text.strip().replace(' ', '_').upper()}_PMAM-",
+            ]
+        if active_selection is None:
+            active_selection = [True] * 3
+        elif isinstance(active_selection, dict):
+            h = active_selection.get("h", False) or active_selection.get("hour", False)
+            m = active_selection.get("m", False) or active_selection.get(
+                "minute", False
+            )
+            pmam = active_selection.get("pmam", False)
+            active_selection = [h, m, pmam]
+
+        label = sg.Text(text, size=(Window.LABEL_SPACING, 1))
+
+        get_values = lambda a, b: [str(x).zfill(2) for x in range(a, b + 1)]
+        e = []
+        if active_selection[0]:
+            e.append(sg.Spin(get_values(1, 12), size=(4, 1), pad=(5, 2), key=keys[0]))
+        if active_selection[1]:
+            e.append(sg.Spin(get_values(0, 59), size=(4, 1), pad=(5, 2), key=keys[1]))
+        if active_selection[2]:
+            e.append(sg.Spin(["AM", "PM"], size=(4, 1), pad=(5, 2), key=keys[2]))
+        return label, *e
+
 
 if __name__ == "__main__":
     window = Window("Scheduler")
@@ -50,7 +124,6 @@ if __name__ == "__main__":
             break
 
         if event == "-CONFIRM-":
-            print(values["-TITLE-"])
-            print(values["-DESCRIPTION-"])
+            print(values)
 
     window.close()
