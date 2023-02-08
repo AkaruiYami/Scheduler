bda8202cdbe69b93546d6c28f1e273c6fdc87cda
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2023-02-08 21:18:47 +0800

    add a method to create data cell

--- timetable.py
+++ timetable.py
@@ -10,6 +10,8 @@ class TimetableWindow(sg.Window):
         self.cell_size = (20, 1)
         self.data.extend(self.headers[1:], [["A"] * 8] * len(self.headers[1:]))
         self.data["Time"] = "1:00:00"
+        self.data["Monday"][1] = ""
+        self.data.update("Tuesday", [""] * 3)
         layout = self._create_layout()
         super().__init__(title, layout, **kwargs)
 
@@ -24,13 +26,10 @@ class TimetableWindow(sg.Window):
             )
             for header in self.data.keys()
         ]
+        cell_color = lambda x: "#ff0a0a" if x != "" else None
         data_row = [
             [
-                sg.Text(
-                    data,
-                    size=(self.cell_size[0], 3),
-                    relief=sg.RELIEF_SUNKEN,
-                )
+                self._create_cell(data, cell_color(data))
                 for data in row
             ]
             for row in self.data.t_values()
@@ -39,6 +38,15 @@ class TimetableWindow(sg.Window):
         layout.append(header_row)
         layout.extend(data_row)
         return layout
+
+    def _create_cell(self, data, color=None):
+        return sg.Text(data,
+                       size=(self.cell_size[0], 3),
+                       relief=sg.RELIEF_SUNKEN,
+                       background_color=color,
+                       expand_x=True, expand_y=True
+                    ) 
+
 
 
 if __name__ == "__main__":
