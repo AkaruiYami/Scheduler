77973cec403c443d5d84c40597384bc51185c1df
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2023-03-13 22:03:53 +0800

    Move layout creator function into layout.py file

---
+++ layout.py
@@ -0,0 +1,116 @@
+import PySimpleGUI as sg
+
+LABEL_SIZE = (10, 1)
+
+
+def header_cell(txt):
+    t = sg.Text(
+        "\n" + txt,
+        relief=sg.RELIEF_RAISED,
+        expand_x=True,
+        size=(15, 3),
+        justification="center",
+    )
+    return t
+
+
+def content_cell(txt):
+    t = sg.Text(txt, relief=sg.RELIEF_SUNKEN, expand_x=True, size=(15, 3))
+    return t
+
+
+def create_timetable_layout(timetable):
+    layout = [[header_cell(txt) for txt in ["Time"] + timetable.days]]
+    for time in timetable.time_frame:
+        layout.append([header_cell(time)] + [ctx for ctx in timetable[time].values()])
+    return layout
+
+
+def create_main_layout(save_files, timetable):
+    # save_files = get_profile(with_prefix=False, with_extension=False)
+    current_file = save_files[0] if save_files else None
+    # top_menu = [["File", ["New Scheme"]], ["Help", ["About"]]]
+    layout = [
+        # [sg.Menu(top_menu)],
+        [
+            sg.Combo(
+                save_files,
+                current_file,
+                size=25,
+                readonly=True,
+                enable_events=True,
+                key="-PROFILE-",
+            ),
+            sg.Push(),
+            sg.Button("Add", key="-ADD_BUTTON-", size=10),
+            sg.Button("Save", key="-SAVE_BUTTON-", size=10),
+            sg.Button("D", key="-DELETE_BUTTON-", button_color="#fe0232", size=3),
+        ],
+    ]
+    timetable_layout = create_timetable_layout(timetable)
+    layout.append(timetable_layout)
+    return layout
+
+
+def create_input_laytout(
+    timetable, d_title="", d_desc="", d_day=0, d_time=0, d_duration=0, edit=False
+):
+    layout = [
+        _text_input("Title", size=LABEL_SIZE, default=d_title),
+        _text_input("Description", size=LABEL_SIZE, default=d_desc),
+        _element_selector("Day", timetable.days, size=LABEL_SIZE, default=d_day),
+        _element_selector(
+            "Time", timetable.time_frame, size=LABEL_SIZE, default=d_time
+        ),
+        _element_selector(
+            "Duration",
+            tuple(range(1, 10)),
+            size=LABEL_SIZE,
+            default=d_duration,
+            visible=not edit,
+        ),
+        [
+            sg.Push(),
+            sg.Button("Cancel", key="-CANCEL-"),
+            sg.Button("Confirm", key="-CONFIRM-"),
+            sg.Button("Delete", key="-DELETE-", visible=edit),
+        ],
+    ]
+    return layout
+
+
+
+
+def _text_input(
+    label, size=(None, None), key=None, default="", input_size=(None, None)
+):
+    if key is None:
+        key = _lkey(label)
+    l = sg.Text(label, size=size)
+    entry = sg.Input(key=key, default_text=default, size=input_size)
+    return l, entry
+
+
+def _element_selector(
+    label, items, size=(None, None), key=None, default=0, visible=True
+):
+    if key is None:
+        key = _lkey(label)
+    if isinstance(default, str):
+        default = items.index(default)
+    l = sg.Text(label, size=size, visible=visible)
+    entry = sg.Combo(
+        items,
+        default_value=items[default],
+        key=key,
+        size=size[0] + 5,
+        readonly=True,
+        visible=visible,
+    )
+    return l, entry
+
+def _lkey(label, *args):
+    label = label.strip().strip("-")
+    if args:
+        label = "-".join([label] + list(args))
+    return f'-{label.upper().replace(" ", "_")}-'
--- main.py
+++ main.py
@@ -6,33 +6,11 @@ import PySimpleGUI as sg
 
 import models
 import ccolors
+import layout as lyt
 
 ROOT_DIR = os.path.dirname(sys.argv[0])
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 timetable = models.Timetable("07:00AM", "06:00PM", 60)
-
-
-def header_cell(txt):
-    t = sg.Text(
-        "\n" + txt,
-        relief=sg.RELIEF_RAISED,
-        expand_x=True,
-        size=(15, 3),
-        justification="center",
-    )
-    return t
-
-
-def content_cell(txt):
-    t = sg.Text(txt, relief=sg.RELIEF_SUNKEN, expand_x=True, size=(15, 3))
-    return t
-
-
-def create_timetable_layout():
-    layout = [[header_cell(txt) for txt in ["Time"] + timetable.days]]
-    for time in timetable.time_frame:
-        layout.append([header_cell(time)] + [ctx for ctx in timetable[time].values()])
-    return layout
 
 
 def get_profile(idx=None, with_prefix=True, with_extension=True):
@@ -56,30 +34,6 @@ def remove_profile(name):
         os.remove(target)
 
 
-def create_main_layout():
-    save_files = get_profile(with_prefix=False, with_extension=False)
-    current_file = save_files[0] if save_files else None
-    layout = [
-        [
-            sg.Combo(
-                save_files,
-                current_file,
-                size=25,
-                readonly=True,
-                enable_events=True,
-                key="-PROFILE-",
-            ),
-            sg.Push(),
-            sg.Button("Add", key="-ADD_BUTTON-", size=10),
-            sg.Button("Save", key="-SAVE_BUTTON-", size=10),
-            sg.Button("D", key="-DELETE_BUTTON-", button_color="#fe0232", size=3),
-        ]
-    ]
-    timetable_layout = create_timetable_layout()
-    layout.append(timetable_layout)
-    return layout
-
-
 def save_timetable(name):
     name = filename_to_savename(name)
     with open(os.path.join(DATA_DIR, name), "wb") as f:
@@ -87,8 +41,7 @@ def save_timetable(name):
 
 
 def load_timetable(name):
-    if not name.endswith(".pkl"):
-        name = name + ".pkl"
+    name = filename_to_savename(name)
     with open(os.path.join(DATA_DIR, name), "rb") as f:
         table = pickle.load(f)
         timetable.load(table)
@@ -102,83 +55,29 @@ def filename_to_savename(name):
     return name
 
 
-window = sg.Window("Scheduler", layout=create_main_layout(), finalize=True)
-
-saves = get_profile()
-if saves:
-    load_timetable(saves[0])
-
-
-def create_window(
-    title, d_title="", d_desc="", d_day=0, d_time=0, d_duration=0, edit=False
-):
-    label_size = (10, 1)
-    layout = [
-        _text_input("Title", size=label_size, default=d_title),
-        _text_input("Description", size=label_size, default=d_desc),
-        _element_selector("Day", timetable.days, size=label_size, default=d_day),
-        _element_selector(
-            "Time", timetable.time_frame, size=label_size, default=d_time
-        ),
-        _element_selector(
-            "Duration",
-            tuple(range(1, 10)),
-            size=label_size,
-            default=d_duration,
-            visible=not edit,
-        ),
-        [
-            sg.Push(),
-            sg.Button("Cancel", key=lkey("Cancel")),
-            sg.Button("Confirm", key="-CONFIRM-"),
-            sg.Button("Delete", key="-DELETE-", visible=edit),
-        ],
-    ]
+def create_window(title, layout):
     window = sg.Window(title, layout=layout)
     return window
 
 
-def _text_input(label, size=(None, None), key=None, default=""):
-    if key is None:
-        key = lkey(label)
-    l = sg.Text(label, size=size)
-    entry = sg.Input(key=key, default_text=default)
-    return l, entry
-
-
-def _element_selector(
-    label, items, size=(None, None), key=None, default=0, visible=True
-):
-    if key is None:
-        key = lkey(label)
-    if isinstance(default, str):
-        default = items.index(default)
-    l = sg.Text(label, size=size, visible=visible)
-    entry = sg.Combo(
-        items,
-        default_value=items[default],
-        key=key,
-        size=size[0] + 5,
-        readonly=True,
-        visible=visible,
-    )
-    return l, entry
-
-
-def lkey(label):
-    return f'-{label.upper().replace(" ", "_")}-'
-
-
 color = ccolors.get_color()
+saves = get_profile(with_prefix=False, with_extension=False)
+window = sg.Window(
+    "Scheduler", layout=lyt.create_main_layout(saves, timetable), finalize=True
+)
+if saves:
+    load_timetable(saves[0])
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
 
     if event == "-ADD_BUTTON-":
-        acw_event, acw_values = create_window("Add New Schedule").read(close=True)
+        acw_event, acw_values = create_window(
+            "Add New Schedule", lyt.create_input_laytout(timetable)
+        ).read(close=True)
         if acw_event == "-CONFIRM-":
-            print(acw_values)
             day = acw_values["-DAY-"]
             time = acw_values["-TIME-"]
             # content = f'{acw_values["-TITLE-"]}\n{acw_values["-DESCRIPTION-"]}'
@@ -215,11 +114,14 @@ while True:
         v = models.Content(*cur_val.get().split("\n"))
         edw_event, edw_values = create_window(
             "Add New Schedule",
-            d_title=v.title,
-            d_desc=v.description,
-            d_day=day,
-            d_time=time,
-            edit=True,
+            lyt.create_input_laytout(
+                timetable,
+                d_title=v.title,
+                d_desc=v.description,
+                d_day=day,
+                d_time=time,
+                edit=True,
+            ),
         ).read(close=True)
         if edw_event == "-CONFIRM-":
             new_day = edw_values["-DAY-"]
